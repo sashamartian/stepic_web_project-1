ece304e3f4fa8bf4bbe82d2acd29ab8a6e40d56e
Nikolay Guriev <stear955@gmail.com>
2017-04-08 22:12:50 +0300

    1.9 changed init.sh and changed nginx.conf

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /etc/gunicorn.d/test hello.py:wsgi_application
