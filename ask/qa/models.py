2e6cd382e5ac1c33665c7559a66170a0a1ffd6af
Nikolay Guriev <stear955@gmail.com>
2017-04-28 17:51:01 +0300

    2.5 modified models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 
 class QuestionManager(models.Manager):
 	def new(self):
-		return self.order_by('-added_at')
+		return self.order_by('-id')
 	def popular(self):
 		return self.order_by('-rating')
 
@@ -16,7 +16,7 @@ class Question(models.Model):
 	objects = QuestionManager()
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateField(auto_now_add=False)
+	added_at = models.DateField(auto_now_add=True)
 	rating = models.IntegerField(default=0)
 	author = models.ForeignKey(User, related_name='author')
 	likes = models.ManyToManyField(User, related_name='likes')
@@ -27,18 +27,12 @@ class Question(models.Model):
 	def get_url(self):
 		return reverse('one_question', kwargs = {'question_id': self.id})
 
-	class Meta:
-		ordering = ['id']
-
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateField(auto_now_add=False)
+	added_at = models.DateField(auto_now_add=True)
 	question = models.ForeignKey(Question)
 	author = models.ForeignKey(User)
 
 	def __str__(self):
 		return self.text
-
-	class Meta:
-		ordering = ['id']
