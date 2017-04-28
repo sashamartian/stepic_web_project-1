1a396e13eb195afdd847128634019567d586efe3
Nikolay Guriev <stear955@gmail.com>
2017-04-28 17:48:08 +0300

    2.5 modified init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo rm -f /etc/nginx/sites-available/default
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo nginx -c /home/box/web/etc/nginx.conf
 
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
